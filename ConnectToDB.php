f3732d70f7f74a0f26fc74ee767a8acb140687d4
Basilliy <z.kon2009@gmail.com>
2016-08-03 17:26:59 +0300

    Create ConnectToDB.php

---
+++ ConnectToDB.php
@@ -0,0 +1,31 @@
+<?php
+class adminPanel{
+    
+public function connectToDB(){
+$host = 'upperl.mysql.ukraine.com.ua'; // адрес сервера 
+$database = 'upperl_vadik'; // имя базы данных
+$user = 'upperl_vadik'; // имя пользователя
+$password = '2shmpzez'; // пароль
+$link = mysqli_connect($host, $user, $password,$database)
+    or die('Не удалось соединиться: ' . mysql_error());
+//echo 'Соединение успешно установлено';
+    return $link;
+}
+    
+public function getKeyboard(){
+$link = connectToDB();
+$query = 'SELECT * FROM menu';
+$result = $link->query($query) or die('Запрос не удался: ' . mysql_error());
+//print_r($result);
+$rows = $result->fetch_assoc();
+//print_r($rows['ButtonsName']);
+//$mystring = 'Generate Insult,Language,Homepage';
+$findme   = ',';
+$button = explode($findme, $rows['ButtonsName']);
+for($i = 0; $i < count($button); $i++){
+    echo '<br>';
+    print_r($button["$i"]);
+}
+    
+}
+}
