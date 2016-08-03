936b76bf73e226a76fcdf4b48a11cc0fbd333b56
Basilliy <z.kon2009@gmail.com>
2016-08-03 17:46:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,43 @@
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
- include "ConnectToDB.php";
+ * and open the template in the editor.*/
+
+class adminPanel{
+
+public function connectToDB(){
+$host = 'upperl.mysql.ukraine.com.ua'; // адрес сервера 
+$database = 'upperl_vadik'; // имя базы данных
+$user = 'upperl_vadik'; // имя пользователя
+$password = '2shmpzez'; // пароль
+$link = mysqli_connect($host, $user, $password,$database)
+    or die('Не удалось соединиться: ' . mysql_error());
+//echo 'Соединение успешно установлено';
+    return $link;
+}
+    
+public function getKeyboard(){
+$link = connectToDB();
+$query = 'SELECT * FROM menu';
+$result = $link->query($query) or die('Запрос не удался: ' . mysql_error());
+//print_r($result);
+$rows = $result->fetch_assoc();
+//print_r($rows['ButtonsName']);
+//$mystring = 'Generate Insult,Language,Homepage';
+$findme   = ',';
+$button = explode($findme, $rows['ButtonsName']);
+for($i = 0; $i < count($button); $i++){
+    echo '<br>';
+    print_r($button["$i"]);
+}
+    
+}
+public function sayHi(){
+print_r("russik say hi");
+    
+}
+} 
  
-$conn = new ConnectToDB;
+$conn = new ConnectToDB();
 $conn->sayHi();
 echo'hi';
