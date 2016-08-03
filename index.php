97c38eaf6eb9c74d15130068f25aad6862019e1a
Basilliy <z.kon2009@gmail.com>
2016-08-03 17:40:11 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+ include "ConnectToDB.php";
+ 
 $conn = new ConnectToDB();
 $conn->sayHi();
 echo'hi';
