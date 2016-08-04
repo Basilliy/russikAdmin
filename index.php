772c4516e10497fc6befbe0e8d94c946b56ff0a2
Basilliy <z.kon2009@gmail.com>
2016-08-04 10:19:11 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <?php
 include "ConnectToDB.php";
-class A{
-  function sayHi(){
-    echo'hi russik';
-  }
-  
-}
 
-$a = new A();
-$a->sayHi();
+//$a = new A();
+//$a->sayHi();
 $b = new ConnectToDB();
-$b->sayHi();
-echo'hi';
+$b->getKeyboard();
+//echo'hi';
