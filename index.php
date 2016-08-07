bfbcee6151dd27df495220f187a0c15d0664fe54
Basilliy <z.kon2009@gmail.com>
2016-08-07 13:18:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,19 +3,6 @@
  <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <title>Пример веб-страницы</title>
-    <style type="text/css">
-   button.new {
-    background: -moz-linear-gradient(#00BBD6, #EBFFFF);
-    background: -webkit-gradient(linear, 0 0, 0 100%, from(#00BBD6), to(#EBFFFF));
-    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#00BBD6', endColorstr='#EBFFFF');
-    padding: 3px 7px;
-    color: #333;
-    -moz-border-radius: 5px;
-    -webkit-border-radius: 5px;
-    border-radius: 5px;
-    border: 1px solid #666;
-   }
-  </style>
  </head>
  <body>
 <?php
@@ -33,7 +20,7 @@ $c = $b->getKeyboard();
 //echo'<br/>';
 $i=0;
 for($i = 0; $i < count($c); $i ++){
-echo"<button class="new">$c[$i]</button>";
+echo"<button onclick = "$b->sayHi()">$c[$i]</button>";
 }
 ?>
  </body>
