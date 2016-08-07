658f03139f75ee28dbf57f591c9bd7a356c42dd1
Basilliy <z.kon2009@gmail.com>
2016-08-07 14:49:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,37 @@
-
 <html>
  <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <title>Пример веб-страницы</title>
+  <style type="text/css">
+   button.new {
+    background: -moz-linear-gradient(#00BBD6, #EBFFFF);
+    background: -webkit-gradient(linear, 0 0, 0 100%, from(#00BBD6), to(#EBFFFF));
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#00BBD6', endColorstr='#EBFFFF');
+    padding: 3px 7px;
+    color: #333;
+    -moz-border-radius: 5px;
+    -webkit-border-radius: 5px;
+    border-radius: 5px;
+    border: 1px solid #666;
+   }
+  </style>
  </head>
  <body>
 <?php
 include "ConnectToDB.php";
-
 //$a = new A();
 //$a->sayHi();
 $b = new ConnectToDB();
 $c = $b->getKeyboard();
 //echo'<br/>';
-
-
 //print_r("russik print");
 //echo'<br/>';
 //echo'<br/>';
 $i=0;
 for($i = 0; $i < count($c); $i ++){
-echo"<button onclick = "$b->sayHi()">$c[$i]</button>";
+    ?>
+<button class="new"><?=$c[$i]?></button>
+<?php
 }
 ?>
  </body>
